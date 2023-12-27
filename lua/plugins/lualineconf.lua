b3b72b91966761c9e99a6a110a97e8b37ba51ba7
Teddy Randby <tedrandby@gmail.com>
2023-12-27 16:06:36 +0000

    Sync and delete

--- lua/plugins/lualineconf.lua
+++ lua/plugins/lualineconf.lua
@@ -53,12 +53,6 @@ return {
       },
       tabline = {
         lualine_a = { "buffers" },
-        lualine_b = {
-        },
-        lualine_c = {},
-        lualine_x = {
-        },
-        lualine_y = {},
         lualine_z = { "tabs" },
       },
     },
